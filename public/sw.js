546c28f4ee2ea583c0421335ad812e1cab436c52
Tyler Gasperlin <gasperlint@gmail.com>
2020-03-29 11:27:10 -0700

    Build out caching and dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -7,9 +7,22 @@ self.addEventListener('install', function(event) {
         //this is what caches content. 
         //think of these as requests not paths
         //needed to cache / so when user navigates to the main page we request the appropriate resource = index.html
-        cache.add('/')
-        cache.add('/index.html')
-        cache.add('/src/js/app.js')
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/src/js/app.js',
+            '/src/js/feed.js',
+            '/src/js/promise.js',
+            '/src/js/fetch.js',
+            '/src/js/material.min.js',
+            '/src/css/app.css',
+            '/src/css/feed.css',
+            '/src/css/help.css',
+            '/src/images/main-image.jpg',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ])
     }))
 })
 
@@ -26,7 +39,15 @@ self.addEventListener('fetch', function(event) {
                 if(response){
                     return response;
                 } else {
-                    return fetch(event.request);
+                    return fetch(event.request)
+                    .then(function(res){
+                        //can call whatever we want - separate from the other cache
+                        return caches.open('dynamic')
+                        .then(function(cache){
+                            cache.put(event.request.url, res.clone())
+                                return res
+                        })
+                    })
                 }
             })
     );
