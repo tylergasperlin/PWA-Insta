719c91a863b1a475ecf91dc3e0cf3931cceb918d
Tyler Gasperlin <gasperlint@gmail.com>
2020-03-28 20:50:31 -0700

    Add support for old browser

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 let deferredPrompt;
 
+if(!window.Promise){
+    window.Promise = Promise
+}
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register('/sw.js')
@@ -24,6 +28,39 @@ var promise = new Promise(function(resolve, reject) {
         // console.log('Called when timer is done')
     }, 3000)
 });
+
+fetch('https://httpbin.org/ip')
+    .then(function(response){
+        console.log(response)
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers:{
+        'Content-Type': 'applicaiton/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Does this work?'})
+})
+    .then(function(response){
+        console.log(response)
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
 
 
 // promise.then(function(text) {
