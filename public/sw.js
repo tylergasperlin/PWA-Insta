53afa426a1df643918e4b791bd9de6d28102e93f
Tyler Gasperlin <gasperlint@gmail.com>
2020-03-29 07:32:07 -0700

    Build out caching main assets

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,14 @@
 self.addEventListener('install', function(event) {
     console.log('[Service worker] Installing service worker...' , event)
-    event.waitUntil(caches.open('static') //can name static or whatever you want
+    event.waitUntil(
+    caches.open('static') //can name static or whatever you want
     .then(function(cache){
         console.log('[Service worker] Precaching app shell')
+        //this is what caches content. 
+        //think of these as requests not paths
+        //needed to cache / so when user navigates to the main page we request the appropriate resource = index.html
+        cache.add('/')
+        cache.add('/index.html')
         cache.add('/src/js/app.js')
     }))
 })
@@ -13,6 +19,16 @@ self.addEventListener('activate', function(event) {
 })
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+        //dont use catch here because the response comes back null if error
+            .then(function(response){
+                if(response){
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
 })
 
