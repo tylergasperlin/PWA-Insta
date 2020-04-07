0f6837cfb11f8596e4a3eb9536e91005371c8418
Tyler Gasperlin <gasperlint@gmail.com>
2020-04-06 23:47:36 -0700

    fix another bug. isinarray would not work for / and /inddex

--- public/sw.js
+++ public/sw.js
@@ -20,13 +20,15 @@ let STATIC_FILES = [
 ];
 
 function isInArray(string, array) {
-    for(var i = 0; i < array.length; i ++) {
-        if(array[1] === string) {
-            return true;
-        }
+    var cachePath;
+    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+      //console.log('matched ', string);
+      cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    } else {
+      cachePath = string; // store the full request (for CDNs)
     }
-    return false;
-}
+    return array.indexOf(cachePath) > -1;
+  }
 
 self.addEventListener('install', function(event) {
     console.log('[Service worker] Installing service worker...' , event)
